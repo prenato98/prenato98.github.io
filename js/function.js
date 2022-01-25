83d39f7946b92634ef92e889a212cca038f8094b
prenato98 <95254517+prenato98@users.noreply.github.com>
2022-01-25 12:27:23 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -172,7 +172,210 @@ markers:false
    gsap.to(".center5", {
 
       scrollTrigger: {
+ScrollTrigger.defaults({
+markers:false
+});
+
+
+
+
+ gsap.to(".center", {
+
+      scrollTrigger: {
+         trigger: ".center",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+//fontSize: "2em"
+     
+
+   });
+   gsap.to(".center", {
+
+      scrollTrigger: {
+         trigger: ".center",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 20%",
+   end: "top 0%",
+   pin:true,
+   whiteSpacing:false,
+      },
+   
+   });
+
+
+
+   gsap.to(".center1", {
+
+      scrollTrigger: {
+         trigger: ".center1",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.3, scaleY:1.3,}
+     
+
+   });
+   gsap.to(".center1", {
+
+      scrollTrigger: {
+         trigger: ".center1",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 20%",
+   end: "top 0%",
+   pin:true,
+   whiteSpacing:false,
+      },
+   
+   });
+
+   gsap.to(".center2", {
+
+      scrollTrigger: {
+         trigger: ".center2",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+     
+
+   });
+   gsap.to(".center2", {
+
+      scrollTrigger: {
+         trigger: ".center2",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 20%",
+   end: "top 0%",
+   pin:true,
+   whiteSpacing:false,
+      },
+   
+   });
+
+   gsap.to(".center3", {
+
+      scrollTrigger: {
+         trigger: ".center3",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+     
+
+   });
+   gsap.to(".center3", {
+
+      scrollTrigger: {
+         trigger: ".center3",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 20%",
+   end: "top 0%",
+   pin:true,
+   whiteSpacing:false,
+      },
+   
+   });
+
+   gsap.to(".center4", {
+
+      scrollTrigger: {
+         trigger: ".center4",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+     
+
+   });
+   gsap.to(".center4", {
+
+      scrollTrigger: {
+         trigger: ".center4",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 20%",
+   end: "top 0%",
+   pin:true,
+   
+      },
+   
+   });
+
+
+   gsap.to(".center5", {
+
+      scrollTrigger: {
          trigger: ".center5",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+     
+
+   });
+   gsap.to(".center5", {
+
+      scrollTrigger: {
+         trigger: ".center5",
+   scrub:true,
+   id: "PIIIIIIIIIIIIINN",
+   start: "top 30%",
+   end: "top 0%",
+   pin:true,
+  
+      },
+   
+   });
+
+   gsap.to(".center6", {
+
+      scrollTrigger: {
+         trigger: ".center6",
+  scrub:true,
+ id: "HHHEEERRREEEE",
+  start: "top 20%",
+  end: "top 0%",
+
+      },
+      
+      css:{ rotation:"0",scaleX:1.5, scaleY:1.5,}
+     
+
+   });
+   gsap.to(".center6", {
+
+      scrollTrigger: {
+         trigger: ".center6",
    scrub:true,
    id: "PIIIIIIIIIIIIINN",
    start: "top 30%",
@@ -211,7 +414,7 @@ markers:false
           end: "200% -200%",
          
       },
-      css:{marginLeft: "-130vw"}
+      css:{marginLeft: "-140vw"}
 
    });
 
